a770a888ec42a5c22031ff840c02fcc97538361a
Calvin Ren <j.calvin.ren@gmail.com>
2024-10-26 19:04:11 +1000

    [feature] android example implement

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spell_checker/flutter_spell_checker.dart';
+import 'package:flutter_spell_checker/models/word_match.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,59 +12,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Spell Checker Example'),
+        ),
+        body: const SpellCheckExample(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class SpellCheckExample extends StatefulWidget {
+  const SpellCheckExample({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SpellCheckExampleState createState() => _SpellCheckExampleState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _SpellCheckExampleState extends State<SpellCheckExample> {
+  String _inputText = "Helo wrld";
+  List<WordMatch> _suggestions = [];
 
-  void _incrementCounter() {
+  void _checkSpelling() async {
+    final result = await FlutterSpellChecker.checkSpelling(_inputText);
+
     setState(() {
-      _counter++;
+      _suggestions = result;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+            onChanged: (text) {
+              _inputText = text;
+            },
+            decoration: const InputDecoration(labelText: 'Enter text to check'),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _checkSpelling,
+            child: const Text('Check Spelling'),
+          ),
+          const SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _suggestions.length,
+              itemBuilder: (context, index) {
+                final suggestion = _suggestions[index];
+                return ListTile(
+                  title: Text('Word: ${suggestion.word}'),
+                  subtitle: Text(
+                      'Suggestions: ${suggestion.replacements.join(', ')}'),
+                );
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          )
+        ],
       ),
     );
   }
