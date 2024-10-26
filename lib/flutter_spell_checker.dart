39ffcb4fd81b7f4efd6d9777cdd147317489d648
Calvin Ren <j.calvin.ren@gmail.com>
2024-10-26 21:12:16 +1000

    Disable web

--- lib/flutter_spell_checker.dart
+++ lib/flutter_spell_checker.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_spell_checker/models/word_match.dart';
 
 import 'flutter_spell_checker_platform_interface.dart';
 
 class FlutterSpellChecker {
   static Future<List<WordMatch>> checkSpelling(String text) {
+    if (kIsWeb) throw UnsupportedError('Web is not supported');
     return FlutterSpellCheckerPlatform.instance.checkSpelling(text);
   }
 }
